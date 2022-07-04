4338a72586c9ad82cefc97f1e46b6d46779f788b
erianvc <erianvc.dev@gmail.com>
2022-07-04 00:50:13 -0500

    refactor: adds tailwind directives types

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,6 @@
 module.exports = {
-  purge: ['./**/*.html'],
-  theme: {
-    screens: {
-      sm: '600px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1440px',
-    },
-    container: {
-      center: true,
-      padding: '2rem',
-    },
-  },
+  content: ['./**/*.{html,css}'],
+  theme: {},
   variants: {},
   plugins: [],
 }
