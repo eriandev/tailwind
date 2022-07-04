f77ccc3a6892db785a4530c9fecf014a26a3ec72
erianvc <erianvc.dev@gmail.com>
2022-07-04 00:26:50 -0500

    style: updates prettiers format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    purge: ['./**/*.html'],
-    theme: {
-        screens: {
-            sm: '600px',
-            md: '768px',
-            lg: '1024px',
-            xl: '1440px',
-        },
-        container: {
-            center: true,
-            padding: '2rem'
-        },
+  purge: ['./**/*.html'],
+  theme: {
+    screens: {
+      sm: '600px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
     },
-    variants: {},
-    plugins: []
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+  },
+  variants: {},
+  plugins: [],
 }
